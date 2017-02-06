932c14c1bc5d10474487e0ee0bdfbea62dd340af
azitou <anas.zitou@soat.fr>
2017-02-06 15:50:37 +0100

    added the fisrt 2 KATAs

---
+++ ConsoleApplication1/ConsoleApplication1/FooBarQix.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ConsoleApplication1
+{
+
+    public class FooBarQixTests
+    {
+        [Fact]
+        public void Should_Get_1_When_pass_1()
+        {
+            Assert.Equal("1", FooBarQix.Process(1));
+        }
+
+        [Fact]
+        public void Should_Get_1_When_pass_2()
+        {
+            Assert.Equal("2", FooBarQix.Process(2));
+        }
+
+        [Fact]
+        public void Should_Get_N_When_pass_Not_Divisible_by_and_not_contain_3_or_5_or_7()
+        {
+            Assert.Equal("16", FooBarQix.Process(16));
+        }
+        [Fact]
+        public void Should_Get_FooFoo_When_pass_3()
+        {
+            Assert.Equal("FooFoo", FooBarQix.Process(3));
+        }
+        [Fact]
+        public void Should_Get_BarBar_When_pass_5()
+        {
+            Assert.Equal("BarBar", FooBarQix.Process(5));
+        }
+        [Fact]
+        public void Should_Get_BarBar_When_pass_7()
+        {
+            Assert.Equal("QixQix", FooBarQix.Process(7));
+        }
+
+        [Fact]
+        public void Should_Get_Foo_When_pass_13()
+        {
+            Assert.Equal("Foo", FooBarQix.Process(13));
+        }
+        [Fact]
+        public void Should_Get_Bar_When_pass_56()
+        {
+            Assert.Equal("Bar", FooBarQix.Process(58));
+        }
+        [Fact]
+        public void Should_Get_Bar_When_pass_74()
+        {
+            Assert.Equal("Qix", FooBarQix.Process(74));
+        }
+
+        [Fact]
+        public void Should_Get_FooBarBar_When_pass_15()
+        {
+            Assert.Equal("FooBarBar", FooBarQix.Process(15));
+        }
+
+        [Fact]
+        public void Should_Get_FooBarBar_When_pass_33()
+        {
+            Assert.Equal("FooFooFoo", FooBarQix.Process(33));
+        }
+
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("2", 2)]
+        [InlineData("FooBar", 51)]
+        [InlineData("BarFoo", 53)]
+        [InlineData("FooFooFooFoo", 333)]
+        [InlineData("FooQixFooBarQix", 357)]
+        public void Should_Get_N_When_Pass_N(string expected, int n)
+        {
+            Assert.Equal(expected, FooBarQix.Process(n));
+        }
+
+    }
+
+    public class FooBarQix
+    {
+        public static string Process(int n)
+        {
+            string containStringResult = "";
+            n.ToString().ToList().ForEach(x => containStringResult += GetContainString(x.ToString()));
+            var result = GetDivisibilityString(n) + containStringResult;
+
+            return String.IsNullOrEmpty(result) ? n.ToString() : result;
+        }
+
+        private static string GetContainString(string i)
+        {
+            var sb = new StringBuilder();
+            if (i.Contains("3"))
+                sb.Append("Foo");
+            if (i.Contains("5"))
+                sb.Append("Bar");
+            if (i.Contains("7"))
+                sb.Append("Qix");
+
+            return sb.ToString();
+
+        }
+
+        private static string GetDivisibilityString(int i)
+        {
+            var sb = new StringBuilder();
+            if (i % 3 == 0)
+                sb.Append("Foo");
+            if (i % 5 == 0)
+                sb.Append("Bar");
+            if (i % 7 == 0)
+                sb.Append("Qix");
+            return sb.ToString();
+        }
+    }
+
+
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+
+
+    }
+
+    public class FizzBuzzTests
+    {
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("2", 2)]
+        public void Should_Get_N_When_Pass_N(string expected, int n)
+        {
+            Assert.Equal(expected, FizzBuzz.Process(n));
+        }
+
+        [Fact]
+        public void Should_Get_Fizz_When_Pass_3()
+        {
+            Assert.Equal("fizz", FizzBuzz.Process(3));
+        }
+
+        [Fact]
+        public void Should_Get_Fizz_When_Pass_5()
+        {
+            Assert.Equal("buzz", FizzBuzz.Process(5));
+        }
+
+        [Theory]
+        [InlineData(15)]
+        [InlineData(30)]
+        public void Should_Get_Fizzbuzz_When_Pass_divible_by_15_N(int n)
+        {
+            Assert.Equal("fizzbuzz", FizzBuzz.Process(n));
+        }
+    }
+
+    public class FizzBuzz
+    {
+        public static string Process(int i)
+        {
+            if (i % 3 == 0)
+            {
+                if (i % 5 == 0)
+                    return "fizzbuzz";
+                return "fizz";
+            }
+
+            if (i % 5 == 0)
+                return "buzz";
+
+            return i.ToString();
+        }
+    }
+}
